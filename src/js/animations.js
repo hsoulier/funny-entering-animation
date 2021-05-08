7c1f656241fcafa21295b83dfcc2d4c0109f99ff
Hippolyte Soulier <hi.soulier@gmail.com>
2021-05-08 21:50:00 +0200

    🚂 Initiate Locomotive Scroll

---
+++ src/js/animations.js
@@ -0,0 +1,93 @@
+import { gsap } from "gsap"
+
+export const opening = (DOM) => {
+	const tl = gsap.timeline({
+		onComplete: () => {
+			DOM.body.style.oveflow = "auto"
+		},
+	})
+	tl.to(DOM.loadImg, {
+		y: "-95vh",
+		stagger: 0.3,
+		duration: 2.8,
+	})
+		.to(
+			DOM.loadImg,
+			{
+				opacity: 0,
+				stagger: 0.3,
+				duration: 0.8,
+			},
+			"-=2.4"
+		)
+		.set(DOM.loadImg, {
+			visibility: "hidden",
+		})
+		.fromTo(
+			DOM.spansTitle,
+			{
+				opacity: 0,
+			},
+			{
+				repeat: 2,
+				ease: "power3.inOut",
+				opacity: 1,
+				duration: 0.14,
+			}
+		)
+		.to(
+			DOM.spansTitle,
+			{
+				width: "auto",
+			},
+			"+=.2"
+		)
+		.to(DOM.title, {
+			ease: "power3.inOut",
+			opacity: 1,
+			yPercent: 0,
+			duration: 1.2,
+			stagger: 0.014,
+		})
+
+		.to(
+			DOM.imgWrapper,
+			{
+				y: 0,
+				duration: 0.8,
+				stagger: 0.3,
+			},
+			"-=.2"
+		)
+		.to(
+			DOM.img,
+			{
+				opacity: 1,
+				y: 0,
+				stagger: 0.3,
+				duration: 0.8,
+			},
+			"-=1.4"
+		)
+}
+
+export const init = (DOM) => {
+	gsap.set(DOM.homeSection, {
+		opacity: 1,
+	})
+	gsap.set(DOM.spansTitle, {
+		width: 0,
+		opacity: 0,
+	})
+	gsap.set(DOM.title, {
+		opacity: 0,
+		yPercent: 100,
+	})
+	gsap.set(DOM.imgWrapper, {
+		y: "81vh",
+	})
+	gsap.set(DOM.img, {
+		y: "-81vh",
+		opacity: 0,
+	})
+}
---
+++ src/js/index.js
@@ -0,0 +1,51 @@
+import { gsap } from "gsap"
+import Splitting from "splitting"
+import { onResize } from "./utils"
+import { Cursor } from "./utils/Cursor"
+import { init, opening } from "./animations"
+import LocomotiveScroll from "locomotive-scroll"
+import "../style/main.scss"
+import "splitting/dist/splitting.css"
+import "splitting/dist/splitting-cells.css"
+import "locomotive-scroll/dist/locomotive-scroll.min.css"
+
+class App {
+	constructor() {
+		Splitting()
+		this.createDOM()
+		this.DOM.body.classList.add("is-loaded")
+		window.addEventListener("resize", onResize)
+		onResize()
+		window.addEventListener("load", () => {
+			const scroll = new LocomotiveScroll({
+				el: document.querySelector("[data-scroll-container]"),
+				smooth: true,
+				direaction: "vertical",
+			})
+			init(this.DOM)
+			setTimeout(() => {
+				this.DOM.body.classList.remove("is-loading")
+				opening(this.DOM)
+			}, 800)
+			this.cursor = new Cursor()
+		})
+	}
+
+	createDOM() {
+		this.DOM = {
+			body: document.body,
+			homeSection: document.querySelector(".home"),
+			title: [...document.querySelectorAll(".home h1 .char")],
+			spansTitle: [
+				...document.querySelectorAll(".home h1 span[data-splitting]"),
+			],
+			loadImg: [...document.querySelectorAll(".opening__img-wrapper")],
+			imgWrapper: [...document.querySelectorAll(".home .img-wrapper")],
+			img: [...document.querySelectorAll(".home .img-wrapper img")],
+		}
+	}
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	const app = new App()
+})
